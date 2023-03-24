d7508d7379178a31a0ed0542d0eede04e3613382
Sami Wazni <63284324+samiwazni@users.noreply.github.com>
2023-03-24 09:46:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Hello World, This is Geoup 7</h2>
+        <h2>Hello World, This is Group 7</h2>
       </header>
     </div>
   );
