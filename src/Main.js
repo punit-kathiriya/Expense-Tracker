6dfa8fcbf949ad612de05040cfbcfd2ee1cd927c
Sami Wazni <63284324+samiwazni@users.noreply.github.com>
2023-03-28 13:02:05 +0300

    Create Main.js

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import './App.css';
+
+function App() {
+    return (
+      <div className="cont">
+        <IncomeExpenses />
+      </div>
+    );
+  }
+
+export default Main;
