a3948d4469c901ad97d0f9443941bf73287114cc
punit4474 <punitkathiriya@gmail.com>
2023-03-31 14:30:29 +0300

    update in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,64 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 export const Home = () => {
   return (
-    <>
-      <h2>Welcome!</h2>
-      <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.</p>
-    </>
+    <Container>
+      <Row className='mt-5'>
+        <Col><h1>Expense Tracker App!</h1></Col>
+      </Row>
+      <Row className='mt-5'>
+        <Col xs={12} md={4}>
+          <Card>
+            <Card.Body>
+              <Card.Title className='header-side'><h2 className='mb-2'>0.00 €</h2>Total Cost</Card.Title>
+            </Card.Body>
+          </Card>
+
+        </Col>
+        <Col xs={12} md={4}>
+          <Card>
+            <Card.Body>
+              <Card.Title className='header-side'><h2 className='mb-2'>0.00 €</h2>Total Consumption (in Ltr)</Card.Title>
+            </Card.Body>
+          </Card>
+
+        </Col>
+        <Col xs={12} md={4}>
+          <Card>
+            <Card.Body>
+              <Card.Title className='header-side'><h2 className='mb-2'>0.00 €</h2>Total Distance (in KM)</Card.Title>
+            </Card.Body>
+          </Card>
+
+        </Col>
+      </Row>
+      <Row className='mt-2'>
+        <Col xs={12} md={4}>
+          <Card>
+            <Card.Body>
+              <Card.Title className='header-side'><h2 className='mb-2'>0.00 €</h2>Average Expence per 100km (in €)</Card.Title>
+            </Card.Body>
+          </Card>
+
+        </Col>
+
+      </Row>
+      <Row className='mt-2'>
+        <Col xs={12} md={12}>
+          <Button href="/main"  variant="primary" size="lg" type="buttton">
+            Add Expence
+          </Button>
+
+        </Col>
+
+      </Row>
+    </Container>
+
   );
 }
 
