287d40974fb57484fc1917ba7f8081e69b7a319c
Sami Wazni <63284324+samiwazni@users.noreply.github.com>
2023-04-01 13:47:59 +0300

    Changed the SignIn.js form

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,29 +1,57 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import { Row, Col } from 'react-bootstrap';
 
 export const SignIn = () => {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email/Phone</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <Row>
+        <Col>
+            <img src='https://mdbootstrap.com/img/new/standard/city/044.webp' className='img-fluid shadow-4' alt='...' />
+        </Col>
+        <Col>
+        <form>
+            <h3>Sign In</h3>
+            <div className="mb-3">
+                <label>Email address</label>
+                <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Enter email"
+                />
+            </div>
+            <div className="mb-3">
+                <label>Password</label>
+                <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Enter password"
+                />
+            </div>
+            <div className="mb-3">
+                <div className="custom-control custom-checkbox">
+                    <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="customCheck1"
+                    />
+                    <label className="custom-control-label" htmlFor="customCheck1">
+                    Remember me
+                    </label>
+                </div>
+            </div>
+            <div className="d-grid">
+                <button type="submit" className="btn btn-primary">
+                    Submit
+                </button>
+            </div>
+            <p className="forgot-password text-right">
+            Forgot <a href="#">password?</a>
+            </p>
+        </form>
+        </Col>
+    </Row>   
   );
 }
 
