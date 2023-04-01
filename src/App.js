b38607f5a59554256cec58e45128765512356eb8
Sami Wazni <63284324+samiwazni@users.noreply.github.com>
2023-04-01 13:50:05 +0300

    Clean up the App.js and added SignIn and Sign Up pages

--- src/App.js
+++ src/App.js
@@ -4,25 +4,31 @@ import { Home } from './components/Home';
 import { Main } from './components/Main';
 import { Cars } from './components/Cars';
 import { SignIn } from './components/SignIn'
+import { SignUp } from './components/SignUp'
 import  AppNav  from './components/AppNav';
 import { API } from './apitest';
 import { Routes, Route } from 'react-router-dom';
-import {AddCars} from './components/AddCar';
+import { AddCars } from './components/AddCar';
+import { Container } from 'react-bootstrap';
 
 
 
 function App() {
   return (
     <div className="App">
-      <AppNav />
-      <Routes>
+      <Container className="text-left">
+        <AppNav />
+        <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/main" element={<Main />} />
         <Route path="/cars" element={<Cars />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/api" element={<API />} />
         <Route path="/cars/add" element={<AddCars />} />
     </Routes>
+      </Container>
+      
     </div>
   );
 }
