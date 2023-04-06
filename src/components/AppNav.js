46dfd0b91a5b3ad543791057c7a8874a033b0843
Sami Wazni <63284324+samiwazni@users.noreply.github.com>
2023-04-06 21:23:19 +0300

    Fixed nav style

--- src/components/AppNav.js
+++ src/components/AppNav.js
@@ -2,22 +2,23 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom"
 
 function AppNav() {
   return (
     <Navbar expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="/">Expense Tracker App</Navbar.Brand>
+        <Navbar.Brand  to="/">Expense Tracker App</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/" className="active">Home</Nav.Link>
-            <Nav.Link href="/main">Main</Nav.Link>
-            <Nav.Link href="/cars">Cars</Nav.Link>
+            <Link to="/" className="active">Home</Link>
+            <Link to="/main">Main</Link>
+            <Link to="/cars">Cars</Link>
           </Nav>
           <Nav>
-            <Nav.Link href="/signin">Sign In</Nav.Link>
-            <Nav.Link href="/signup">Sign Up</Nav.Link>
+            <Link to="/signin">Sign In</Link>
+            <Link to="/signup">Sign Up</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
