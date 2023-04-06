15de78f418aed423d5f5372c6c2056a415999ad9
Sami Wazni <63284324+samiwazni@users.noreply.github.com>
2023-04-06 22:50:41 +0300

    Update AppNav.js

--- src/components/AppNav.js
+++ src/components/AppNav.js
@@ -4,7 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom"
 
-function AppNav() {
+export const AppNav = () => {
   return (
     <Navbar expand="lg" bg="dark" variant="dark">
       <Container>
