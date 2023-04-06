6d550d3894102312409a367cf832480b47d7043e
Sami Wazni <63284324+samiwazni@users.noreply.github.com>
2023-04-07 01:20:50 +0300

    modified cars file so when we add car will be display in the page

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { CiCircleMinus } from "react-icons/ci";
-import { CiEdit } from "react-icons/ci";
-import { CiCirclePlus } from "react-icons/ci";
+import { CiCircleMinus } from 'react-icons/ci';
+import { CiEdit } from 'react-icons/ci';
+import { CiCirclePlus } from 'react-icons/ci';
 import { useNavigate, useLocation } from 'react-router-dom';
 
 export const Cars = () => {
   const location = useLocation();
-  const formData = location.state?.formData;
+  const formData = location.state;
 
   // Array of cars that were added from the form
-  const addedCars = formData?.addedCars || [];
+  const [addedCars, setAddedCars] = useState(formData ? [formData] : []);
+
+  const handleRemove = (id) => {
+    setAddedCars(addedCars.filter((car) => car.id !== id));
+  };
 
   return (
     <>
       <Row className='mt-5'>
-        <Col xs={12} md={8}><h1>Car List</h1></Col>
+        <Col xs={12} md={8}>
+          <h1>Car List</h1>
+        </Col>
         <Col xs={12} md={4}>
-          <Button href="/cars/add" variant="primary" type="buttton">
+          <Button href='/cars/add' variant='primary' type='buttton'>
             Add Car <CiCirclePlus />
           </Button>
         </Col>
@@ -34,25 +40,25 @@ export const Cars = () => {
                   <Row>
                     <Col xs={12} md={8}>
                       <Card.Title className='header-side'>
-                        <h2 className='mb-2'>{car.name}</h2>
-                        {car.manufacture}
+                        <h2 className='mb-2'>{car.carName}</h2>
+                        {car.carManufacture}
                       </Card.Title>
                     </Col>
                     <Col xs={12} md={4}>
                       <Button
                         href={`/car/edit/${car.id}`}
                         className='mb-3'
-                        variant="primary"
-                        size="xs"
-                        type="button"
+                        variant='primary'
+                        size='xs'
+                        type='button'
                       >
                         Edit <CiEdit />
                       </Button>
                       <Button
-                        href={`/car/remove/${car.id}`}
-                        variant="danger"
-                        size="xs"
-                        type="button"
+                        variant='danger'
+                        size='xs'
+                        type='button'
+                        onClick={() => handleRemove(car.id)}
                       >
                         Remove <CiCircleMinus />
                       </Button>
