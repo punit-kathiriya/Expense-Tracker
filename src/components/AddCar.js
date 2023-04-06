2765899ec91cc22614b8687dbe105c49731e0e18
Sami Wazni <63284324+samiwazni@users.noreply.github.com>
2023-04-07 01:21:29 +0300

    Modified addcar form to send added car to the car page

--- src/components/AddCar.js
+++ src/components/AddCar.js
@@ -1,48 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { CiCirclePlus } from "react-icons/ci";
+import { useNavigate } from 'react-router';
 
-export const AddCars = () => {
-    return ( 
-        <>
-        <Row>
-            <Col className='mt-5 expense-data'>
-                <h2>Add Car</h2>
-                <Form>
-                    <Form.Group className="mb-3" controlId="formBasic">
-                        <Form.Label>Car Name </Form.Label>
-                        <Form.Control type="text" placeholder="Enter Your Car Name" id="name" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasic">
-                        <Form.Label>Car Manufacture</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Car Manufacture" id='manufacture' />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasic">
-                        <Form.Label>Fuel Capacity (in ltr)</Form.Label>
-                        <Form.Control type="number" min="0" placeholder="Enter Fuel Capacity (in ltr)" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasic">
-                        <Form.Label>Car Bettery (in %)</Form.Label>
-                        <Form.Control type="number" min="0" placeholder="Enter Car Bettery (in %)" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasic">
-                        <Form.Check type="checkbox" id="is-electirc" label="is your car Electic?" />
-                    </Form.Group>
+export const AddCar = () => {
+  const [carName, setCarName] = useState('');
+  const [carManufacture, setCarManufacture] = useState('');
+  const [fuelCapacity, setFuelCapacity] = useState(0);
+  const [carBattery, setCarBattery] = useState(0);
+  const [isElectric, setIsElectric] = useState(false);
+  const navigate = useNavigate();
 
-                    <Button variant="primary" type="submit">
-                        Add Car <CiCirclePlus />
-                    </Button>
-                </Form>
-            </Col>
-            <Col xs lg="5">
-            
-            </Col>
-        </Row>
-        </>
-    );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = { carName, carManufacture, fuelCapacity, carBattery, isElectric };
+    navigate('/cars', { state: formData });
+  };
+  
+  return ( 
+    <>
+      <Row>
+        <Col className='mt-5 expense-data'>
+          <h2>Add Car</h2>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasic">
+              <Form.Label>Car Name </Form.Label>
+              <Form.Control type="text" placeholder="Enter Your Car Name" id="name" value={carName} onChange={(e) => setCarName(e.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasic">
+              <Form.Label>Car Manufacture</Form.Label>
+              <Form.Control type="text" placeholder="Enter Car Manufacture" id='manufacture' value={carManufacture} onChange={(e) => setCarManufacture(e.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasic">
+              <Form.Label>Fuel Capacity (in ltr)</Form.Label>
+              <Form.Control type="number" min="0" placeholder="Enter Fuel Capacity (in ltr)" value={fuelCapacity} onChange={(e) => setFuelCapacity(e.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasic">
+              <Form.Label>Car Battery (in %)</Form.Label>
+              <Form.Control type="number" min="0" placeholder="Enter Car Battery (in %)" value={carBattery} onChange={(e) => setCarBattery(e.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasic">
+              <Form.Check type="checkbox" id="is-electirc" label="is your car Electric?" checked={isElectric} onChange={(e) => setIsElectric(e.target.checked)} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Add Car <CiCirclePlus />
+            </Button>
+          </Form>
+        </Col>
+        <Col xs lg="5"></Col>
+      </Row>
+    </>
+  );
 };
 
-export default AddCars;
+export default AddCar;
