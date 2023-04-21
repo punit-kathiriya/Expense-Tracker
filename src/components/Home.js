071864493fcd135ddae0d8e61e6db70fc8158621
Sami Wazni <63284324+samiwazni@users.noreply.github.com>
2023-04-21 20:58:02 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,9 +12,9 @@ export const Home = () => {
     <>
     <div className="homepage-container">
       <Container className="my-5">
-          <h1>Welcome to Expense Tracker App</h1>
+          <h2>Welcome to Expense Tracker App</h2><br></br>
           <p>Car Fuel Expenses application to help families calculate the fuel expenses of their cars. This application dynamically calculates and presents various information related to the refueling expenses of cars.</p>
-          <div>
+          <div> <br></br>
             <Button href="signin" variant="primary">Sign In</Button> { }
             <Button href="signup" variant="outline-secondary">Sign Up</Button>
           </div>
