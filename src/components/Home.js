712500034a5c1fa33ea83b7515e3162156bc6c3a
punit4474 <punitkathiriya@gmail.com>
2023-04-21 10:52:16 +0300

    Dashboard work done and Prompt issue solved

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,35 @@
 // Auth: Punit
 // Desc: This file containing code for the Expense Tracker to display the prices
 
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { Row, Col, Card, Button } from 'react-bootstrap';
 import { CiCirclePlus } from "react-icons/ci";
 
+
 export const Home = () => {
+  const [total, setTotal] = useState([]);
+  // const [consumption, setConsumption] = useState([]);
+  // const [distance, setDistance] = useState([]);
+  // const [expence, setExpence] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/api/mileage_prices")
+      .then((response) => response.json())
+      .then((data) => setTotal(data));
+  }, []);
+
+  let totalcost = 0;
+
+  total.map((items) => (totalcost += items.Total_price));
+
+  let totalConsumption = 0;
+  total.map((items) => (totalConsumption += items.Total_filled));
+  let totalDistance = 0;
+  total.map((items) => (totalDistance += items.Total_distance));
+
+  let totalAverage = ((totalcost * 100) / totalDistance).toFixed(2);;
+
+
   return (
     <>
       <Row className='mt-5'>
@@ -22,7 +46,7 @@ export const Home = () => {
         <Col xs={12} md={4} className="mt-2">
           <Card>
             <Card.Body>
-              <Card.Title className='header-side'><h2 className='mb-2'>0.00 €</h2>Total Cost</Card.Title>
+              <Card.Title className='header-side'><h2 className='mb-2'>{totalcost} €</h2>Total Cost</Card.Title>
             </Card.Body>
           </Card>
         </Col>
@@ -30,7 +54,7 @@ export const Home = () => {
         <Col xs={12} md={4} className="mt-2">
           <Card>
             <Card.Body>
-              <Card.Title className='header-side'><h2 className='mb-2'>0.00 €</h2>Total Consumption (in Ltr)</Card.Title>
+              <Card.Title className='header-side'><h2 className='mb-2'>{totalConsumption} ltr.</h2>Total Consumption (in Ltr)</Card.Title>
             </Card.Body>
           </Card>
         </Col>
@@ -38,7 +62,7 @@ export const Home = () => {
         <Col xs={12} md={4} className="mt-2">
           <Card>
             <Card.Body>
-              <Card.Title className='header-side'><h2 className='mb-2'>0.00 €</h2>Total Distance (in KM)</Card.Title>
+              <Card.Title className='header-side'><h2 className='mb-2'>{totalDistance} KM</h2>Total Distance (in KM)</Card.Title>
             </Card.Body>
           </Card>
         </Col>
@@ -46,7 +70,7 @@ export const Home = () => {
         <Col xs={12} md={4} className="mt-2">
           <Card>
             <Card.Body>
-              <Card.Title className='header-side'><h2 className='mb-2'>0.00 €</h2>Average Expence per 100km (in €)</Card.Title>
+              <Card.Title className='header-side'><h2 className='mb-2'>{totalAverage} €</h2>Average Expence per 100km (in €)</Card.Title>
             </Card.Body>
           </Card>
         </Col>
